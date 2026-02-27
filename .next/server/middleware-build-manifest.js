5c03829a280706561375fbe2eae0a94de9302f4d
CYTAdmin <chooseyourtherapist@gmail.com>
2026-02-27 12:12:34 -0800

    Fix: Remove Vercel deploy from GitHub Actions and update install command

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -23,6 +23,16 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
+    ],
+    "/therapy-booking": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/therapy-booking.js"
+    ],
+    "/view-all-therapist": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/view-all-therapist.js"
     ]
   },
   "ampFirstPages": []
