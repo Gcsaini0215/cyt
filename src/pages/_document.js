72a2671252632782963e4911be5f519fc3abcfd2
Deepak <dpk7697@gmail.com>
2026-08-13 01:23:55 +0530

    Add Google Tag Manager container for marketing tracking
    Installs GTM-WTX77RFB site-wide so GA4/Meta Pixel and future
    marketing tags can be configured from the GTM dashboard without
    further code changes.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const GTM_ID = 'GTM-WTX77RFB';
+
 export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
@@ -11,8 +13,26 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;800;900&display=swap" rel="stylesheet" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
       </Head>
       <body suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="gtm"
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
